edadcca22ccd2b16156a432939c9c602af10b3e4
Vadim Inshakov <vadiminshakov@gmail.com>
2023-08-10 20:22:32 +0500

    make partitioner exportable

--- crcpartitioner.go
+++ crcpartitioner.go
@@ -2,11 +2,15 @@ package partmap
 
 import "hash/crc32"
 
-type hashSumPartitioner struct {
+type HashSumPartitioner struct {
 	partitionsNum uint
 }
 
-func (h *hashSumPartitioner) Find(key string) (uint, error) {
+func NewHashSumPartitioner(partitionsNum uint) *HashSumPartitioner {
+	return &HashSumPartitioner{partitionsNum: partitionsNum}
+}
+
+func (h *HashSumPartitioner) Find(key string) (uint, error) {
 	hashSum := crc32.ChecksumIEEE([]byte(key))
 
 	return uint(hashSum) % h.partitionsNum, nil
