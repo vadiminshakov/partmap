7229877d8146772635908fc7a25cd428f62440a3
Vadim Inshakov <vadiminshakov@gmail.com>
2023-08-10 20:12:23 +0500

    extract partition

---
+++ partition.go
@@ -0,0 +1,25 @@
+package main
+
+import "sync"
+
+type partition struct {
+	stor map[string]any
+	sync.RWMutex
+}
+
+func (p *partition) set(key string, value any) {
+	p.Lock()
+	p.stor[key] = value
+	p.Unlock()
+}
+
+func (p *partition) get(key string) (any, bool) {
+	p.RLock()
+	v, ok := p.stor[key]
+	if !ok {
+		p.RUnlock()
+		return nil, false
+	}
+	p.RUnlock()
+	return v, true
+}
