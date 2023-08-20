aab5ddfaa800843dac17037d6b8d412774904670
Vadim Inshakov <vadiminshakov@gmail.com>
2023-08-20 21:13:35 +0500

    add del test

--- partmap_test.go
+++ partmap_test.go
@@ -5,12 +5,17 @@ import (
 	"testing"
 )
 
-func TestSetGet(t *testing.T) {
+func TestSetGetDel(t *testing.T) {
 	m := NewPartitionedMap(NewHashSumPartitioner(1000), 1000)
 	require.NoError(t, m.Set("1", 1))
 	v, err := m.Get("1")
 	require.NoError(t, err)
 	require.Equal(t, 1, v)
+
+	require.NoError(t, m.Del("1"))
+	v, err = m.Get("1")
+	require.ErrorIs(t, err, ErrNotFound)
+	require.Nil(t, v)
 }
 
 func TestSetEmptyKey(t *testing.T) {
@@ -18,13 +23,13 @@ func TestSetEmptyKey(t *testing.T) {
 	require.ErrorIs(t, m.Set("", 1), ErrEmptyKey)
 }
 
-func TestSetEmptyValue(t *testing.T) {
-	m := NewPartitionedMap(NewHashSumPartitioner(1000), 1000)
-	require.ErrorIs(t, m.Set("1", nil), ErrEmptyValue)
-}
-
 func TestGetEmptyKey(t *testing.T) {
 	m := NewPartitionedMap(NewHashSumPartitioner(1000), 1000)
 	_, err := m.Get("")
 	require.ErrorIs(t, err, ErrEmptyKey)
+}
+
+func TestDelEmptyKey(t *testing.T) {
+	m := NewPartitionedMap(NewHashSumPartitioner(1000), 1000)
+	require.ErrorIs(t, m.Del(""), ErrEmptyKey)
 }
