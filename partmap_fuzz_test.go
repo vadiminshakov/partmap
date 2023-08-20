aa121ecbbe7898797682e1e30e4d45cdab2d2d5b
Vadim Inshakov <vadiminshakov@gmail.com>
2023-08-20 13:01:04 +0500

    add fuzzing test

---
+++ partmap_fuzz_test.go
@@ -0,0 +1,27 @@
+package partmap
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func FuzzPartMap(f *testing.F) {
+	m := NewPartitionedMap(NewHashSumPartitioner(1000), 1000)
+
+	f.Add("key", 1)
+	f.Add("1", 2)
+	f.Fuzz(func(t *testing.T, key string, value int) {
+		err := m.Set(key, value)
+		if err == ErrEmptyKey || err == ErrEmptyValue {
+			t.Skip()
+		}
+		require.NoError(t, err)
+
+		v, err := m.Get(key)
+		if err == ErrEmptyKey {
+			t.Skip()
+		}
+		require.NoError(t, err)
+		require.Equal(t, value, v)
+	})
+}
