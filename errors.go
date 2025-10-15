14ea5546b0d2261326b08649d9ed5744d5009a6b
Vadim Inshakov <vadiminshakov@gmail.com>
2025-10-16 02:35:00 +0500

    add errors

--- errors.go
+++ errors.go
@@ -7,4 +7,8 @@ var (
 	ErrEmptyKey = errors.New("empty key provided")
 	// ErrNotFound is returned when key is not found in map.
 	ErrNotFound = errors.New("key not found")
+	// ErrNilPartitioner is returned when partitioner is nil.
+	ErrNilPartitioner = errors.New("partitioner must not be nil")
+	// ErrInvalidPartitions is returned when partitions number is zero.
+	ErrInvalidPartitions = errors.New("number of partitions must be greater than zero")
 )
